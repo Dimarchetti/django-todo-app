d22aaeb3ffd506b5960439b849548ebc0302346c
Dimitri Marchetti <dimitri.marchetti@gmail.com>
2021-10-18 01:26:04 -0300

    Feature Tasks: model and view ok

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Task) # Cria campos automáticos no admin gerados através do model criado
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import *
 
 # Create your views here.
+
+# Função que conotrola a apresentação da view tasks/list.html
+
+def index(request):
+  tasks = Task.objects.all() # pega todos os objetos tasks
+
+  context =  {'tasks': tasks} # Coloca tasks em um dicionário para poder renderizar na view
+  return render(request, 'tasks/list.html', context) # context como um terceiro parâmetro para poder renderizar na view
