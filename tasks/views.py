9559ca5ced639ef886c85e215df1ad9c6ddb46bd
Dimitri Marchetti <dimitri.marchetti@gmail.com>
2021-11-08 18:05:17 -0300

    Features create and update ok

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,41 @@
-from django.shortcuts import render
+from django.db import reset_queries
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 
-# Função que conotrola a apresentação da view tasks/list.html
+# Função que controla a apresentação do HTML tasks/list.html
 
 def index(request):
   tasks = Task.objects.all() # pega todos os objetos tasks
+  form = TaskForm()
 
-  context =  {'tasks': tasks} # Coloca tasks em um dicionário para poder renderizar na view
-  return render(request, 'tasks/list.html', context) # context como um terceiro parâmetro para poder renderizar na view
+# Fazer as tasks aparecerem na view
+  if request.method == 'POST':
+    form = TaskForm(request.POST)
+    if form.is_valid():
+      form.save()
+    return redirect('/')
+
+  context =  {'tasks': tasks, 'form': form} # Coloca tasks em um dicionário e em uma variável para poder renderizar na view
+  return render(request, 'tasks/list.html', context) # renderiza a view tasks/list.html com context como um terceiro parâmetro
+
+
+
+def updateTask(request, pk):
+  task = Task.objects.get(id=pk)
+
+  form = TaskForm(instance=task)
+  
+  if request.method =='POST':
+    form = TaskForm(request.POST, instance=task)
+    if form.is_valid():
+      form.save()
+      return redirect('/')
+    
+  context = {'form':form}
+
+  return render(request, 'tasks/update_task.html', context)
